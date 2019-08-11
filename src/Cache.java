7d55f5c4b2fafb6dc5f41c28b18864cc072a45ac
Samvid Mistry <samvid@cse.iitk.ac.in>
2019-08-11 17:49:52 +0530

    Added comments.

--- src/Cache.java
+++ src/Cache.java
@@ -1,3 +1,11 @@
+/**
+ * The most abstract idea of Cache. A cache can just be asked to load an item.
+ */
 public interface Cache {
+    /**
+     * Load or fetch the provided address.
+     * @param address address to be fetched
+     * @return true if address was present in cache, false otherwise
+     */
     boolean load(final String address);
 }
